6a9add4f65bc7cd64791fa6b9035b1091a4fa03e
gabrielvalencia <gabev13@gmail.com>
2022-11-18 09:10:50 -0600

    Part 2 complete: Persisting Employers and Skills

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,45 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotNull
+    @Size(max=100)
+    private String location;
+
+    public Employer() {}
+
+    Employer(String location) {
+        this();
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Employer employer = (Employer) o;
+        return Objects.equals(location, employer.location);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), location);
+    }
 
 
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,42 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max = 200)
+    private String description;
+
+    public Skill() {
+    }
+
+    Skill(String description) {
+        this();
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Skill skill = (Skill) o;
+        return description.equals(skill.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), description);
+    }
 }
